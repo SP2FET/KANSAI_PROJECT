8ef54c99e948793c38ad948ba7167821d125b03f
Unknown <s165355@student.pg.gda.pl>
2019-02-11 19:02:02 +0100

    calculating distance

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 frameQueueLength = 2;
 frameQueue = []
 img4 = 0
-
+maxMatches = 10
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -52,13 +52,30 @@ while(True):
         matches = bf.match(des1, des2)
 
         # Sort them in the order of their distance.
-        matches = sorted(matches, key=lambda x: 1/(x.distance + 1))
+        matches = sorted(matches, key=lambda x: x.distance)
 
         # Draw first 10 matches.
-        img3 = cv2.drawMatches(frameQueue[0], kp1, frameQueue[1], kp2, matches[:10],img4,flags=2)
+        img3 = cv2.drawMatches(frameQueue[0], kp1, frameQueue[1], kp2, matches[:maxMatches],img4,flags=2)
+        coords = []
 
-        cv2.imshow('frame', img3)
+        # getting coordinates of matched points
+        coords1 = [kp1[mat.queryIdx].pt for mat in matches]
+        coords2 = [kp2[mat.trainIdx].pt for mat in matches]
+            #print("x: %s y: %s "% (x,y))
 
+
+        # drawing matched points
+        for marker in coords1[:maxMatches]:
+            img4 = cv2.drawMarker(img3, tuple(int(i) for i in marker), color=(0, 255, 255))
+
+        cv2.imshow('frame', img4)
+
+        # obtaining distance
+        dist_x = (coords1[0][0] - coords2[0][0])
+        dist_y = (coords1[0][1] - coords2[0][1])
+        if dist_x != 0:
+
+            print("distance: %d,%d" % (dist_x,dist_y) )
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
